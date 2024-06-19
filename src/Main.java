1d41a37144ed2a3574b46dc7eedff00239e30231
Angela Dakay <angdkay@gmail.com>
2024-06-19 13:11:57 -0600

    Learning basics of JFrame
    Learn how to create a frame, set title, dimensions, resizing, exiting, creating an Image Icon, and changing the color of the frame.

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,6 @@
-import javax.swing.JFrame; // this import is needed to make a frame
+import java.awt.Color; // this import is needed to make a frame
+import javax.swing.ImageIcon; // imports ImageIcon
+import javax.swing.JFrame; // lets us get colors
 
 public class Main {
     public static void main(String[] args) {
@@ -10,8 +12,15 @@ public class Main {
         frame.setTitle("My first GUI"); // sets title of frame
         frame.setSize(420,420); // sets x-dimension and y-dimension of frame
 
+        frame.setResizable(false); // prevent frame from being resize
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // so now when we hit the x button it will exit the program
         frame.setVisible(true); // makes the frame visible
+
+        ImageIcon image = new ImageIcon("chickicon.png"); // creates an image icon
+        frame.setIconImage(image.getImage()); // change icon of frame
+
+        // frame.getContentPane().setBackground(new Color(12, 32, 12)); // change color of frame, can use GUI
+        frame.getContentPane().setBackground(new Color(0xCCE8B8)); // can also use hexidecimal number
 
 
     }
