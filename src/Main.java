81e18870cfc929fbb4906b1d0cd20c65e354ea91
Angela Dakay <angdkay@gmail.com>
2024-06-26 10:09:17 -0600

    Learnt about Buttons and Border Layout
    Finished the simple sad chicken GUI, moved on to learning more about layout manager.

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,18 @@
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 public class Main {
     public static void main(String[] args) {
-        /* 
-        many ways to create a frame, example below creates an instance of frame and change its properties
+        /* Basics of making a GUI
             
-            import java.awt.Color; // this import is needed to make a frame
+            import java.awt.Color; // this import is needed to change a frames color
             import javax.swing.ImageIcon; // imports ImageIcon
-            import javax.swing.JFrame; // lets us get colors
+            import javax.swing.JFrame; // needed to make a frame
 
             // JFrame = GUI window to add components to
 
@@ -22,13 +27,127 @@ public class Main {
 
                 ImageIcon image = new ImageIcon("chickicon.png"); // creates an image icon
                 frame.setIconImage(image.getImage()); // change icon of frame
-                frame.getContentPane().setBackground(new Color(0xCCE8B8)); // can use hexidecimal number
+                frame.getContentPane().setBackground(new Color(0xCCE8B8)); // can use hexidecimal number for color of frame
 
-        another way is to use JFrame as a parent class to a child class     
+            another way to make it is to use JFrame as a parent class to a child class MyFrame    
         */
 
-        MyFrame frame = new MyFrame();
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        /* JPanel = a GUI component that functions as a container to hold other components
 
+            // Can add Images and Label to our JPanel, we will not be adding this to our JPanels, however
+            ImageIcon icon = new ImageIcon("chickicon.png"); 
+            JLabel label = new JLabel(); 
+            label.setText("Hi");  
+            label.setIcon(icon);  
+            label.setVerticalAlignment(JLabel.CENTER); 
+            label.setHorizontalAlignment(JLabel.CENTER); // would use if we were using borderlayout
+        */
+
+        // Creating JPanels for sadchicken GUI
+        JPanel wallShadow = new JPanel(); // creates new panel
+        wallShadow.setBackground(new Color(0xD3C163)); // set colour of panel
+        wallShadow.setBounds(0, 0, 250, 255); // set size of panel
+
+        JPanel wall = new JPanel();
+        wall.setBackground(new Color(0xE6DC77));
+        wall.setBounds(0, 0, 450, 320);
+
+        JPanel greenForeground = new JPanel();
+        greenForeground.setBackground(new Color(0xCCE8B8));
+        greenForeground.setBounds(0,250, 250, 250);
+        // panelThree.setLayout(new BorderLayout()); this is a border layout, another way to determine where text should go
+        // label.setBounds(0, 0, 200, 200); // sets where text goes
+        // panelThree.add(label); // adds text + image to the panel
+        
+        MyFrame frame = new MyFrame();
+        frame.add(wallShadow);
+        frame.add(wall);
+        frame.add(greenForeground);
+
+        /* Next we learn about border layout
+         * Layout manager = Defines tha natural layout for components within a container [3 types]
+         * BorderLayout [1st type] = A Border Layout places component in five areas: NORTH, WEST, EAST, SOUTH, CENTER 
+         * 
+         * expand proportionately...
+         *      north + south: horizontally [can use north as title and south as footer]
+         *      west + east: vertically [can use west or east as sidebars / navigation bars]
+         *      center: to size of frame [used as main body of your program]
+         */
+
+        // Let's create a new frame to test out border layouts
+        JFrame frame2 = new JFrame();
+        frame2.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame2.setSize(500, 500);
+        frame2.setLayout(new BorderLayout(10, 10)); // sets up new border layout, can send margin of width + height
+        frame2.setVisible(true);
+
+        JPanel panel1 = new JPanel();
+        JPanel panel2 = new JPanel();
+        JPanel panel3 = new JPanel();
+        JPanel panel4 = new JPanel();
+        JPanel panel5 = new JPanel();
+
+        panel1.setBackground(new Color(0xE5FDEE));
+        panel2.setBackground(new Color(0xFEFED9));
+        panel3.setBackground(new Color(0xD9FEF9));
+        panel4.setBackground(new Color(0xF5D9FE));
+        panel5.setBackground(new Color(0xFFFFFF));
+
+        panel1.setPreferredSize((new Dimension(100,100)));
+        panel2.setPreferredSize((new Dimension(100,100)));
+        panel3.setPreferredSize((new Dimension(100,100)));
+        panel4.setPreferredSize((new Dimension(100,100)));
+        panel5.setPreferredSize((new Dimension(100,100)));
+
+        // using borderlayouts to put these panels to their designated places
+        frame2.add(panel1, BorderLayout.NORTH);
+        frame2.add(panel2, BorderLayout.WEST);
+        frame2.add(panel3, BorderLayout.EAST);
+        frame2.add(panel4, BorderLayout.SOUTH);
+        frame2.add(panel5, BorderLayout.CENTER);
+
+        //------------------ sub panels = can add panels within a panel -------------------//
+        JPanel panel6 = new JPanel();
+        JPanel panel7 = new JPanel();
+        JPanel panel8 = new JPanel();
+        JPanel panel9 = new JPanel();
+        JPanel panel10 = new JPanel();
+
+        panel6.setBackground(new Color(0xE5FDEE));
+        panel7.setBackground(new Color(0xFEFED9));
+        panel8.setBackground(new Color(0xD9FEF9));
+        panel9.setBackground(new Color(0xF5D9FE));
+        panel10.setBackground(new Color(0xFFFFFF));
+
+        panel5.setLayout(new BorderLayout());
+
+        panel6.setPreferredSize((new Dimension(50,50)));
+        panel7.setPreferredSize((new Dimension(50,50)));
+        panel8.setPreferredSize((new Dimension(50,50)));
+        panel9.setPreferredSize((new Dimension(50,50)));
+        panel10.setPreferredSize((new Dimension(50,50)));
+
+        panel5.add(panel6, BorderLayout.NORTH);
+        panel5.add(panel7, BorderLayout.WEST);
+        panel5.add(panel8, BorderLayout.EAST);
+        panel5.add(panel9, BorderLayout.SOUTH);
+        panel5.add(panel10, BorderLayout.CENTER);
+        //-----------------------------------------------------------------------------------//
+
+        /* Layout Manager = Defines natural layout for components within a container
+         * FlowLayout [second type] = places components in row, sized at preffered size. If
+         *                            horizontal space in container too small, FlowLayout
+         *                            class uses next available row.
+         */
+
+         JFrame frame3 = new JFrame();
+         frame3.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+         frame3.setSize(500, 500);
+         frame3.setLayout(new FlowLayout());
+         frame3.setVisible(true);
+
+         frame3.add(new JButton("1"));
+
+        
     }
 }
--- src/MyFrame.java
+++ src/MyFrame.java
@@ -1,59 +1,114 @@
 import java.awt.Color;
 import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import javax.swing.BorderFactory;
 import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.border.Border;
 
-public class MyFrame extends JFrame{ // MyFrame is a sublass of the parent class Jframe
+public class MyFrame extends JFrame implements ActionListener{ 
+    // extends makes MyFrame a sublass of parent class Jframe
+    // implement adds a method from ActionListener
 
-    public MyFrame() {
+    JButton button;
+    JLabel friendLabel; // label that makes friend pop up after clicking button to hang out
+    JLabel objective; // label that has the objective of GUI which is to make chicken happy
+    JLabel ending;
 
-        // JLabel = GUI display area for a string of text, an image, or both
+    public MyFrame() { 
 
-        // add label to a frame
-        JLabel labels = new JLabel(); // creates label
-        labels.setText("make chicken happy :D"); // set text of label
+        /* creating a button */
+        // ImageIcon buttonIcon = new ImageIcon("chickicon"); // can add image to buttons
+        button = new JButton(); 
+        button.setBounds(150, 350, 100, 50); 
 
-        labels.setHorizontalTextPosition(JLabel.CENTER); // set text LEFT, CENTER, RIGHT of ImageIcon
-        labels.setVerticalTextPosition(JLabel.TOP); // set text TOP,CENTER,BOTTOM of ImageIcon
-        labels.setForeground(new Color(0x9E6B1D)); // sets color of text
-        labels.setFont(new Font("Mali", Font.PLAIN, 20)); // sets font of text
+        /*customizing the button*/
+        button.setText("Hang out");
+        button.setFocusable(false); // removes random border around text
+        // button.setIcon(buttonIcon); button.setIconTextGap(); 
+        button.setHorizontalTextPosition(JButton.CENTER);
+        button.setVerticalTextPosition(JButton.CENTER); 
+        button.setFont(new Font("Mali", Font.BOLD, 10)); 
+        button.setForeground(Color.white); // forgeround is text color
+        button.setBackground(new Color(0xD3C163));
+        button.setBorder(BorderFactory.createEtchedBorder()); // gives a 3d effect to the button
+        this.add(button); 
 
-        labels.setIconTextGap(0); // set gap of text to image
-        // labels.setBackground(Color.black); // set background color
-        // labels.setOpaque(true); // to display background color
-        labels.setVerticalAlignment(JLabel.CENTER); // set vertical position of icon + text within label
-        labels.setHorizontalAlignment(JLabel.CENTER); // set horizontal position of icon + text within label
+        /* display a label on our frame after clicking a button */
+        friendLabel = new JLabel();
+        ImageIcon friend = new ImageIcon("friend.gif");
+        friendLabel.setIcon(friend);
+        friendLabel.setBounds(14, 0, 400, 400);
+        friendLabel.setVisible(false);
+        this.add(friendLabel);
+        button.addActionListener(this); // friend now pops up after button is hit
+        // button.addActionListener(e -> System.out.println("poo")); // action listener lamda expression shortcut
 
-        // add image to a frame
+        /* JLabel = GUI display area for a string of text, an image, or both */
+        objective = new JLabel(); // creates label
+        objective.setText("make chicken happy :D"); // set text of label
+
+        /* customizing text of JLabel */
+        objective.setHorizontalTextPosition(JLabel.CENTER); // set text LEFT, CENTER, RIGHT of ImageIcon
+        objective.setVerticalTextPosition(JLabel.TOP); // set text TOP,CENTER,BOTTOM of ImageIcon
+        objective.setForeground(new Color(0x9E6B1D)); // sets color of text
+        objective.setFont(new Font("Mali", Font.BOLD, 20)); // sets font of text
+
+        objective.setIconTextGap(0); // set gap of text to image
+        // objective.setBackground(Color.black); // set background color
+        // objective.setOpaque(true); // to display background color
+        objective.setVerticalAlignment(JLabel.CENTER); // set vertical position of icon + text within label
+        objective.setHorizontalAlignment(JLabel.CENTER); // set horizontal position of icon + text within label
+        
+        /* add image to JLabel */
         ImageIcon chicken = new ImageIcon("sadchick.png"); // creates image
-        labels.setIcon(chicken); 
-
+        objective.setIcon(chicken); // adds it to JLabel
         // creating a border
         Border border = BorderFactory.createLineBorder(new Color(0xEEE7D0), 30);
-        //0xECC949 #EEE7D0
-        labels.setBorder(border);
-
-        this.add(labels); // adds the label and image we made earlier
-
-        labels.setText("Hi!");
+        objective.setBorder(border);
         
-        this.setTitle("My first GUI");
-        this.setSize(420,420);
-        this.setResizable(true);
+        /* Ending Jlabel */
+        ending = new JLabel();
+        ending.setText("Chicken is now happy :)");
+        ending.setBounds(88,0, 400, 148);
+        ending.setForeground(new Color(0xEFFF7F)); // sets color of text
+        ending.setFont(new Font("Mali", Font.BOLD, 20)); // sets font of text
+        ending.setVisible(false); // makes Ending Jlabel invisible
+        
+        // adds the ending and objective labels to JFrame
+        this.add(ending);
+        this.add(objective); // adds the objective label w/ text + image we made earlier
+
+        /* JFrame customization */
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setTitle("Sad Chicken");
+        this.setSize(420,500);
+        this.setResizable(false);
         this.setVisible(true);
 
         ImageIcon image = new ImageIcon("chickicon.png");
         this.setIconImage(image.getImage()); // change icon of frame
         this.getContentPane().setBackground(new Color(0xCCE8B8));
-
-        //this.setLayout(null); // wont show the labels 
-        //labels.setBounds(0, 0 , 400, 250); // determines whole labels parameter
+        // this.setLayout(null); // wont show the labels 
+        // objective.setBounds(0, 0 , 400, 250); // determines whole labels parameter
+        
         /* another way to change labels parameter */
-        this.pack(); // make sure you add all components then pack
+        // this.pack(); // make sure you add all components then pack 
+    }
 
-       
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if(e.getSource() == button) {
+            button.setEnabled(false); // disables a button
+            friendLabel.setVisible(true); // after hitting button, friend image pops up
+            objective.setText(null);
+            objective.setIcon(null);
+            ending.setVisible(true);
+            this.setTitle("Happy Chicken");
+        }
+        throw new UnsupportedOperationException("Unimplemented method 'actionPerformed'");
     }
 }
